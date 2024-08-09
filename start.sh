c099f6243429096918e6516ae899c38637211ec6
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-09 13:31:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get your HF_TOKEN from hugging face
-export HF_TOKEN=<YOUR_HF_TOKEN>
+# export HF_TOKEN=<YOUR_HF_TOKEN>
 
 # Start the TGI server
 text-generation-launcher --model-id $MODEL_ID --port $PORT --num-shard $NUM_SHARD --max-input-length $MAX_INPUT_LENGTH --max-total-tokens $MAX_TOTAL_TOKENS
