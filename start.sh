158df751c49d760da23cde7318e2661ab4903bcb
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-09 18:50:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,10 +3,7 @@
 # export HF_TOKEN=<YOUR_HF_TOKEN>
 
 # Start the TGI server
-text-generation-launcher --model-id $MODEL_ID --port $PORT --num-shard $NUM_SHARD --max-input-length $MAX_INPUT_LENGTH --max-total-tokens $MAX_TOTAL_TOKENS
-
-# Wait for the TGI server to start
-sleep 30
+text-generation-launcher --model-id $MODEL_ID --port $PORT --num-shard $NUM_SHARD --max-input-length $MAX_INPUT_LENGTH --max-total-tokens $MAX_TOTAL_TOKENS &
 
 # Start the Flask app
 uvicorn app:app --host 0.0.0.0 --port 7000 --reload
